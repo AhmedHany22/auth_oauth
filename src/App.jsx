87b24a713701cf0ed84baff29cc39c46a2069969
Ahmed Hany <ahmed_hany19@outlook.com>
2023-05-09 18:12:41 +0300

    Auth0

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Profile from "./components/Profile";
+import LoginBtn from "./components/loginBtn";
+import LogoutBtn from "./components/logoutBtn";
+import { useAuth0 } from "@auth0/auth0-react";
+
+function App() {
+  const { isLoading, error } = useAuth0();
+
+  return (
+    <main className="column">
+      <h1>Auth0 Login</h1>
+      {error && <p>Authentication Error</p>}
+      {!error && isLoading && <p>Loading...</p>}
+      {!error && !isLoading && (
+        <>
+          <LoginBtn />
+          <LogoutBtn />
+          <Profile />
+        </>
+      )}
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/LoginBtn.jsx
@@ -0,0 +1,9 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LoginBtn = () => {
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+
+  return !isAuthenticated && <button onClick={() => loginWithRedirect()}>Sign In</button>;
+};
+
+export default LoginBtn;
